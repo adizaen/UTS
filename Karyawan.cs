c0e5a15e475f09e14347d7d8008135b1d1a76d94
Adi Zaenul Mustaqim <adi.mustaqim@students.amikom.ac.id>
2019-04-25 13:06:33 +0700

    ResponsiPemrog2269

---
+++ Karyawan.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrog2269
+{
+    class Karyawan
+    {
+        // Properties
+        public string Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+
+        // Konstruktor
+        public Karyawan(string nik, string nama, int gajibulanan)
+        {
+            Nik = nik;
+            Nama = nama;
+            if(gajibulanan <= 0)    // Jika gaji bulanan < 0, akan di set menjadi 0
+            {
+                GajiBulanan = 0;
+            }
+            else
+            {
+                GajiBulanan = gajibulanan;
+            }
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrog2269
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Gaji Karyawan";
+            // Membuat Objek
+            Karyawan kr1 = new Karyawan("00098712", "Paijo", 2000000);
+            Karyawan kr2 = new Karyawan("00098715", "Paimo", 3000000);
+
+            Header();
+            Console.WriteLine("1.\t{0}/{1}\t\t{2}", kr1.Nik, kr1.Nama, kr1.GajiBulanan);
+            Console.WriteLine("2.\t{0}/{1}\t\t{2}", kr2.Nik, kr2.Nama, kr2.GajiBulanan);
+
+            Console.WriteLine("\nAsikkk, gajiku naik 10%\n");
+            Header();
+            int total1, total2;
+            total1 = kr1.GajiBulanan + ((kr1.GajiBulanan * 10) / 100);
+            total2 = kr2.GajiBulanan + ((kr2.GajiBulanan * 10) / 100);
+
+            Console.WriteLine("1.\t{0}/{1}\t\t{2}", kr1.Nik, kr1.Nama, total1);
+            Console.WriteLine("2.\t{0}/{1}\t\t{2}", kr2.Nik, kr2.Nama, total2);
+
+            Console.ReadKey();
+        }
+        static void Header()
+        {
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("No.\tNik/Nama\t\tGaji Bulanan");
+            Console.WriteLine("--------------------------------------------");
+        }
+    }
+}
